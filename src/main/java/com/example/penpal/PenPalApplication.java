174a5fc40dfc461e11022643646820af1b056aac
KimSangHyeok <tkdgur0906@naver.com>
2023-06-20 16:21:54 +0900

    first commit

---
+++ src/main/java/com/example/penpal/PenPalApplication.java
@@ -0,0 +1,13 @@
+package com.example.penpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenPalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PenPalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/penpal/PenPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.penpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenPalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
