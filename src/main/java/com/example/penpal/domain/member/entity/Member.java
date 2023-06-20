bc1b76c6af1922c9fd8e0115989420d713d51f88
KMK_HM <kyun9151@gmail.com>
2023-06-21 00:02:17 +0900

    feat: Member Entity 작성 및 Swagger 의존성 추가

--- src/main/java/com/example/penpal/domain/member/entity/Member.java
+++ src/main/java/com/example/penpal/domain/member/entity/Member.java
@@ -1,10 +1,48 @@
 package com.example.penpal.domain.member.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.example.penpal.domain.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Member {
+public class Member extends BaseTimeEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private Gender gender;
+
+    @Column(nullable = false)
+    private LocalDate birthday;
+
+    private String profileText;
+    private String profileImage;
+    private String location;
+
+    @Builder
+    public Member(String email, String password, String nickname, Gender gender, LocalDate birthday) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.gender = gender;
+        this.birthday = birthday;
+    }
+
 }
---
+++ src/main/java/com/example/penpal/domain/member/service/MemberService.java
@@ -0,0 +1,7 @@
+package com.example.penpal.domain.member.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+}
