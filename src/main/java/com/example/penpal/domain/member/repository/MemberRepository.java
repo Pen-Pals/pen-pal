495d739ecec06fa81d8fc81a8d4b651d28814301
KimSangHyeok <tkdgur0906@naver.com>
2023-07-07 16:09:21 +0900

    Resolve merge conflict

--- src/main/java/com/example/penpal/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/penpal/domain/member/repository/MemberRepository.java
@@ -9,16 +9,14 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-<<<<<<< HEAD
 
+    Optional<Member> findByEmail(String email);
+    boolean existsByEmail(String email);
     @Query("select m from Member m where m.id in (select l.receiveId from Letter l where l.sendId = :sendId)")
     List<Member> findReceiversBySendId(@Param("sendId") Long sendId);
 
     @Query("select m1 from Member m1 where m1.id in " +
             "(select distinct m2.id from Member m2 join Letter l on m2.id = l.member.id where l.receiveId = :receiveId)")
     List<Member> findSendersByReceiveId(@Param("receiveId") Long receiveId);
-=======
-    Optional<Member> findByEmail(String email);
-    boolean existsByEmail(String email);
->>>>>>> 130bd0e649233f5b1e1330e1482fcfaaf6bf51e2
+
 }
