00042d7f595b357e65a5d9da011a2991b68ffb80
KimSangHyeok <tkdgur0906@naver.com>
2023-08-10 17:06:18 +0900

    docs: main controler 스웨거 작성

--- src/main/java/com/example/penpal/web/letter/MainController.java
+++ src/main/java/com/example/penpal/web/letter/MainController.java
@@ -2,6 +2,8 @@ package com.example.penpal.web.letter;
 
 import com.example.penpal.domain.letter.service.LetterService;
 import com.example.penpal.web.letter.model.LetterRemainTimeListDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,9 @@ public class MainController {
 
     private final LetterService letterService;
 
+    @Operation(summary = "내게 오고있는 편지들 남은 배송시간 보기",
+            description = "아직 나에게 도착하지 않은 편지들의 남은 일, 시간, 분을 알려줌. 배송완료된(남은 시간이 0인) 편지는 표시되지 않음")
+    @ApiResponse(responseCode = "200", description = "오고 있는 편지 조회 성공")
     @GetMapping
     public ResponseEntity<LetterRemainTimeListDto> remainTime(){
         LetterRemainTimeListDto letterRemainTimeListDto = letterService.calculateRemainingTime(getCurrentMemberId());
