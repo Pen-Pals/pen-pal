60c7adeb7dddab2ad1696673d8edf39de472b18c
KMK_HM <kyun9151@gmail.com>
2023-08-14 22:53:55 +0900

    fix: 띄어쓰기

--- src/main/java/com/example/penpal/web/letter/model/DeliveryTimeDto.java
+++ src/main/java/com/example/penpal/web/letter/model/DeliveryTimeDto.java
@@ -13,10 +13,11 @@ public class DeliveryTimeDto {
 
     private int days;
     private int hours;
+
     private int mins;
 
     public static DeliveryTimeDto from(double distance) {
-        double deliveryTime = Math.round(distance) / 100.00;
+        double deliveryTime = Math.round(distance) / 100.00; // 시속 100KM
         int days = (int) deliveryTime / 24;
         deliveryTime -= days * 24;
         int hours = (int) deliveryTime;
