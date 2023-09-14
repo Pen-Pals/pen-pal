5600b33a47e34113db74099e89349db5cfedfc31
KMK_HM <kyun9151@gmail.com>
2023-09-14 09:17:58 +0900

    fix: CORS 에러 해결

--- src/main/java/com/example/penpal/global/security/SecurityConfig.java
+++ src/main/java/com/example/penpal/global/security/SecurityConfig.java
@@ -4,9 +4,11 @@ import com.example.penpal.global.jwt.JwtAccessDeniedHandler;
 import com.example.penpal.global.jwt.JwtAuthenticationEntryPoint;
 import com.example.penpal.global.jwt.JwtSecurityConfig;
 import com.example.penpal.global.jwt.TokenProvider;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,6 +16,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @RequiredArgsConstructor
@@ -36,11 +42,24 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }))
                 .exceptionHandling((e) -> e.authenticationEntryPoint(jwtAuthenticationEntryPoint))
                 .exceptionHandling((e) -> e.accessDeniedHandler(jwtAccessDeniedHandler))
                 .sessionManagement((s) -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .headers((e) -> e.frameOptions((a) -> a.sameOrigin()))
                 .authorizeHttpRequests(request -> request.requestMatchers("api/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**/*").permitAll()
                         .anyRequest().authenticated())
                 .apply(new JwtSecurityConfig(tokenProvider));
 
